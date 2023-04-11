fe15c39930280d924598bfa527ed51c10d5a788f
Jhon Garcia <jhongg6@hotmail.com>
2023-04-11 10:38:12 -0500

    Se implementa boton para abrir el mapa con la ubicación de la universidad.

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -10,7 +10,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GoogleMap mMap;
+    private Button btnMap;
 
     Button buttonOperacion;
     EditText editNumOperacion;
@@ -30,6 +40,27 @@ public class MainActivity extends AppCompatActivity {
                 validacionVentana(numOperacion, view);
             }
         });
+
+        btnMap = findViewById(R.id.btn_map);
+
+        // Agregar un Listener al botón
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para abrir la actividad del mapa
+                Intent mapIntent = new Intent(MainActivity.this, MapsActivity.class);
+
+                // Agregar la ubicación específica en un objeto Bundle
+                Bundle args = new Bundle();
+                LatLng location = new LatLng(4.653384977826347, -74.14522083254803); // San Francisco
+                args.putParcelable("location", location);
+                mapIntent.putExtra("bundle", args);
+
+                // Iniciar la actividad del mapa
+                startActivity(mapIntent);
+            }
+        });
+
     }
 
     public void validacionVentana(int operacion, View view){
